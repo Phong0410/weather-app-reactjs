a387b42e68244fe270b35895398d50ca41be3d44
Phong0410 <17520895@gm.uit.edu.vn>
2022-08-22 14:36:47 +0700

    fix empty image

--- src/components/NoData/index.jsx
+++ src/components/NoData/index.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import styles from "./index.module.css"
+import empty from "../../assets/imgs/empty.png"
 
 const NoData = () => {
 	return (
 		<>
 			<div className={styles.noData}>Not thing to see here</div>
-			<img src="/empty.png" alt="empty" className={styles.emptyImg} />
+			<img src={empty} className={styles.emptyImg} alt="empty" />
 		</>
 	)
 }
